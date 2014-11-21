31b52eff6ba119f82068ceb2111ec14f8b04b7aa
Brien Colwell <xcolwell@gmail.com>
2014-11-20 19:53:09 -0800

    API iteration

--- android/src/main/java/io/nextop/client/android/ConnectionContext.java
+++ android/src/main/java/io/nextop/client/android/ConnectionContext.java
@@ -1,15 +1,69 @@
 package io.nextop.client.android;
 
 
+import android.net.Uri;
 import android.support.annotation.Nullable;
+import rx.Observable;
 
 import java.io.IOException;
 
-// TODO has a transport in it
-// TODO however, tests want to set a transport that is a f
 public interface ConnectionContext {
-    Connection get();
-    Connection get(Message m);
+    Observable<Message> send(Uri p, Message m);
+    // receive is always load balanced
+    // the same path is sent to the same client, for consistency on same objects
+    Observable<Message> receive(Uri p);
+    // always auto ack when observer completes
+//    void ack(Uri id);
+    // seems like a rare case
+//    void cancel(Uri id);
+
+
+    // the control host is "publickeyfingerprint.nextop.io", which is the namespace for message ids
+    // e.g. publickeyfingerprint.nextop.io/ID
+    String getControlHost();
+
+
+
+    // INTROSPECTION
+
+    Observable<MessageStatus> listMessages();
+    Observable<MessageStatus> messageStatus(Uri id);
+//    Observable<Message> get(Uri id);
+
+    Observable<Metrics> metrics(Uri p);
+
+    // a bind is when a message ID is connected to a path
+    // the bind is open/subscribed as long at both sides are connected.
+    // each side can time out or have a subscription->0 transition.
+    // in those cases, the bind is closed
+    Observable<Bind> listBinds(Uri p);
+    Observable<ReceivePath> listReceivePaths();
+
+
+
+    public final class MessageStatus {
+        static enum CompletionStatus {
+            ACKED,
+            CANCELED,
+            PENDING
+        }
+
+        final Uri id;
+        final CompletionStatus completionStatus;
+
+        // TODO id, path, size, transfer stats
+    }
+
+    public static final class Bind {
+        final Uri id;
+        final boolean open;
+    }
+
+    public static final class ReceivePath {
+        final String path;
+        final boolean open;
+    }
+
 
     void close();
 
@@ -50,5 +104,10 @@ public interface ConnectionContext {
 //        float upBytesPerSecond;
 //        float upPacketLoss;
 //    }
+
+
+
+
+
 
 }
