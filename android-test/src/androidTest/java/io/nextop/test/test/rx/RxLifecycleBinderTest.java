aca5dfc9e65dfeb45aeaf0134b6f3389075dfd83
Brien Colwell <xcolwell@gmail.com>
2015-01-23 22:18:10 -0800

    bug report: update docs
    wtf Android Studio

--- android-test/src/androidTest/java/io/nextop/test/test/rx/RxLifecycleBinderTest.java
+++ android-test/src/androidTest/java/io/nextop/test/test/rx/RxLifecycleBinderTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 // TODO wtf is going on with intellij detecting changes here?
 // TODO e.g. make a change, it does not get packaged into the build
 // TODO related to? https://code.google.com/p/android/issues/detail?id=76539
-// TODO see https://code.google.com/p/android/issues/detail?id=109078&thanks=109078&ts=1422062654
+// TODO see my bug https://code.google.com/p/android/issues/detail?id=109078&thanks=109078&ts=1422062654
 public class RxLifecycleBinderTest extends TestCase {
 
     // FIXME
