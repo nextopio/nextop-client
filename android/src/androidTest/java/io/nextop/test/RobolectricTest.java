7910c7fc45f87749a21f180371bbcd6105a4ede2
Brien Colwell <xcolwell@gmail.com>
2015-02-03 22:59:16 -0800

    wip: in android, jarjar is actually useful
    mesa a dev tool

---
+++ android/src/androidTest/java/io/nextop/test/RobolectricTest.java
@@ -0,0 +1,16 @@
+package io.nextop.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class RobolectricTest {
+
+    @Test
+    public void test1() throws Exception {
+
+    }
+}
