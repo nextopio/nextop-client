3bcd0ae891256a81e26b2b9bd4dc08a67e5455ff
Brien Colwell <xcolwell@gmail.com>
2015-02-26 23:17:16 -0800

    nextop: wip

--- java-common/src/main/java/io/nextop/client/node/nextop/NextopNode.java
+++ java-common/src/main/java/io/nextop/client/node/nextop/NextopNode.java
@@ -1,4 +1,47 @@
 package io.nextop.client.node.nextop;
 
-public class NextopNode {
+import io.nextop.client.node.AbstractMessageControlNode;
+
+// FIXME base on a wire factory
+// FIXME use a wire adapter factory
+public class NextopNode extends AbstractMessageControlNode {
+
+
+    // big assumption for ordering: nextop endpoint will not crash
+    // compromise: maintain order and never lose a message if this is true
+    // if not true, at least never lose a message (but order will be lost)
+
+
+
+    // shared transfer state:
+    // id -> bytes, sent index in bytes
+
+    // socket control flow:
+    // - retake timeout (use take state, time since last take, elapsed)
+    // - create wire (socket) (on timeout, go to [0])
+    // - initial handshakes
+    // - initial state sync (sync the shared transfer state)
+    // - start loopers
+    // - when any loopers fails, shut down all, go to [0]
+    //
+
+    // WriteLooper
+    // take off the top of mcs and write
+    // have a parallel thread that peeks at the next
+    // every yieldQ write, surface progress, check if there is a more urgent message
+    // if so, shelve the current and switch
+
+    // ReadLooper
+
+
+
+    // wire format:
+    // [type][length]
+    // types:
+    // - message start [ID]
+    // - message data [bytes]
+    // - message end [MD5]
+    // - (verify error) (ack) (on ack, delete from shared transfer state)
+
+
 }
